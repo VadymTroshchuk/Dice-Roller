28a35261056ed121ce8fd84b05064968e7189e92
Vadym Troshchuk <vadimtrosuk01@gmail.com>
2023-10-15 16:30:23 +0300

    add simple unit test

--- app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
@@ -4,14 +4,14 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun testNumbers(){
+    val dice = Dice(6)
+    val diceResult = dice.roll()
+
+        assertTrue("The expected value should be between 1 and 6" ,
+            diceResult in 1..6)
     }
 }
