920a9557ea942514e54d1417ace8849cbac3a184
Vadym Troshchuk <vadimtrosuk01@gmail.com>
2023-10-15 14:40:07 +0300

    implement dice image change according to roll

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -13,8 +14,8 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.button1)
         rollButton.setOnClickListener {
 
-            val resultTextView: TextView = findViewById(R.id.textView3)
-            resultTextView.text = "6"
+
+
 
         }
 
@@ -25,8 +26,22 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(6)
 
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView3)
-        resultTextView.text = diceRoll.toString()
+
+        val diceImage : ImageView = findViewById(R.id.imageView)
+
+        val randomDiceImage = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(randomDiceImage)
+
+        diceImage.contentDescription = diceRoll.toString()
+
+
 
     }
 
